2a2f1e59870bc00c90d45a1779d5bd0fb5a29859
ktakeuchi <takevadogu@outlook.jp>
2022-03-07 23:11:46 +0900

    Add css modules　

--- compornents/Layout.js
+++ compornents/Layout.js
@@ -1,16 +1,19 @@
 import Head from "next/head";
-import { Children } from "react/cjs/react.development";
-const name = "kuniSan";
+import { children } from "react/cjs/react.development";
+import styles from "./layout.module.css";
+import utilStyles from "../styles/utils.module.css";
+
+const name = "Kuni san";
 export const siteTitle = "Next.js Blog";
 function Layout({ children }) {
     return (
-        <div>
+        <div className={styles.container}>
             <Head>
                 <link rel="icon" href = "/favicon.ico" />
             </Head>
-            <header>
-                <img src = "/images/profile.png" />
-                <h1>{name}</h1>
+            <header className={styles.header}>
+                <img src = "/images/profile.png" className={utilStyles.borderCircle} />
+                <h1 className={utilStyles.heading2Xl}>{name}</h1>
             </header>
             <main>{children}</main>
         </div>
