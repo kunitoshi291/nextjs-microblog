43c8961255ab00426b2c7d8b08cd61b2c54e0459
ktakeuchi <takevadogu@outlook.jp>
2022-03-08 23:36:31 +0900

    ブログのトップページのレイアウト

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,77 @@ import Image from 'next/image'
 import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import Layout from '../compornents/Layout'
+import utilStyle from "../styles/utils.module.css";
 
 export default function Home() {
-  return <Layout></Layout>
+  //ブログ構成のレイアウト
+  return <Layout>
+    <section className = {utilStyle.headingMd}>
+      <p>
+        趣味のプログラミングを通して、学んだことをアウトプットします。
+      </p>
+    </section>
+    <section>
+      <h2>📝エンジニアのブログ</h2>
+      <div className={styles.grid}>
+      <article>
+        <Link href="/" passHref>
+        <img src="/images/thumbnail01.jpg" 
+        className={styles.thumbnailImage}
+        />
+        </Link>
+        <Link href="/" passHref>
+          <a className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </a>
+        </Link>
+        <br />
+        <small className={utilStyle.lightText}>March 9, 2022</small>
+      </article>
+      <article>
+        <Link href="/" passHref>
+        <img src="/images/thumbnail01.jpg" 
+        className={styles.thumbnailImage}
+        />
+        </Link>
+        <Link href="/" passHref>
+          <a className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </a>
+        </Link>
+        <br />
+        <small className={utilStyle.lightText}>March 9, 2022</small>
+      </article>
+      <article>
+        <Link href="/" passHref>
+        <img src="/images/thumbnail01.jpg" 
+        className={styles.thumbnailImage}
+        />
+        </Link>
+        <Link href="/" passHref>
+          <a className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </a>
+        </Link>
+        <br />
+        <small className={utilStyle.lightText}>March 9, 2022</small>
+      </article>
+      <article>
+        <Link href="/" passHref>
+        <img src="/images/thumbnail01.jpg" 
+        className={styles.thumbnailImage}
+        />
+        </Link>
+        <Link href="/" passHref>
+          <a className={utilStyle.boldText}>
+            SSGとSSRの使い分けの場面はいつなのか？
+          </a>
+        </Link>
+        <br />
+        <small className={utilStyle.lightText}>March 9, 2022</small>
+      </article>
+    </div>
+    </section>
+    
+  </Layout>
 }
